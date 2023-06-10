37b84fb49eea9001de876315d34ea7c4a59f877c
EderV <ederv1009@gmail.com>
2023-06-11 00:00:51 +0200

    Added login module and login component with login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {LoginService} from "../global/services/login.service";
+import {UserLogin, UserToken} from "../global/models/User";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  userLogin: UserLogin = {username: '', password: ''}
+
+  constructor(private readonly loginService: LoginService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  submitLogin() {
+    this.loginService.login(this.userLogin).subscribe(
+      (userToken: UserToken) => console.log('Success', userToken),
+      (error) => console.log('Error received', error)
+    )
+  }
 
 }
