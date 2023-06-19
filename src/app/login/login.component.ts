ea967a226659921ffb6407c9ec9eeda3bd103ddf
EderV <ederv1009@gmail.com>
2023-06-19 22:52:51 +0200

    Added session storage for JWT Added http interceptor for authorization header

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {LoginService} from "../global/services/login.service";
 import {UserLogin, UserToken} from "../global/models/User";
+import {SessionStorageService} from "../global/services/session-storage.service";
+import {compareSegments} from "@angular/compiler-cli/src/ngtsc/sourcemaps/src/segment_marker";
 
 @Component({
   selector: 'app-login',
@@ -11,17 +13,27 @@ export class LoginComponent implements OnInit {
 
   userLogin: UserLogin = {username: '', password: ''}
 
-  constructor(private readonly loginService: LoginService) { }
+  constructor(
+    private readonly loginService: LoginService,
+    private readonly sessionStorageService: SessionStorageService
+  ) { }
 
   ngOnInit(): void {
 
   }
 
   submitLogin() {
-    this.loginService.login(this.userLogin).subscribe(
-      (userToken: UserToken) => console.log('Success', userToken),
-      (error) => console.log('Error received', error)
-    )
+    this.loginService.login(this.userLogin).subscribe({
+      next: (userToken: UserToken) => {
+        this.sessionStorageService.setItem('user', userToken)
+        // TODO Redirect to admin panel
+      },
+      error: (error) => console.log('Error in login: ', error)
+    })
   }
 
+  checkSession() {
+    console.log('User:')
+    console.log(this.sessionStorageService.getItem('user'))
+  }
 }
